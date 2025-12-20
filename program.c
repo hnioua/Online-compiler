b66787133941bedeafdd5eaad04ca3f948f8e10c
Hnioua <abdessamadhnioua@gmail.com>
2025-12-20 15:52:53 +0000

    test connexion git hub

--- program.c
+++ program.c
@@ -1,18 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int a, b;
-
-    printf("Entrez le 1er nombre: ");
-    fflush(stdout);  // vide le buffer pour forcer l'affichage immédiat du texte
-    scanf("%d", &a);
-    printf("Vous avez saisi : %d\n", a);
-
-    printf("Entrez le 2e nombre: ");
-    fflush(stdout);
-    scanf("%d", &b);
-    printf("Vous avez saisi : %d\n", b);
-
-    printf("\nRésultat final : %d et %d\n", a, b);
-    return 0;
+int main(){
+	printf("hello world");
+	return 0;
 }
